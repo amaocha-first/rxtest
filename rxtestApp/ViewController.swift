2bb3721077428e1684d0072f859833b215701fc9
NishizawaShota <nishizawa@emol.jp>
2020-02-06 14:49:25 +0900

    Initial Commit

---
+++ rxtestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxtestApp
+//
+//  Created by Shota Nishizawa on 2020/02/06.
+//  Copyright © 2020 Shota Nishizawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
